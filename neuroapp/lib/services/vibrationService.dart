b0c3e90eb6404c99b3ca2dbf1796a96eba456e63
Oliver Klukas <oliverklukas@t-online.de>
2023-06-24 22:28:54 +0200

    Add sound and vibration status

--- neuroapp/lib/services/vibrationService.dart
+++ neuroapp/lib/services/vibrationService.dart
@@ -1,10 +1,22 @@
 import 'package:vibration/vibration.dart';
 
 class VibrationService {
-  static Future<void> vibrate({int duration = 1}) async {
+  static Future<void> vibrateForDuration({int duration = 1}) async {
     var hasVibration = await Vibration.hasVibrator();
     if (hasVibration != null && hasVibration) {
       Vibration.vibrate(duration: duration * 1000);
     }
   }
+
+  static Future<void> startVibration() async {
+    var hasVibration = await Vibration.hasVibrator();
+    if (hasVibration != null && hasVibration) {
+      Vibration.vibrate();
+    }
+  }
+
+  static void stopVibration() {
+    Vibration.cancel();
+  }
+
 }
