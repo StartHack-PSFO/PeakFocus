50add03ba175ff198bb572b4d42ffaf56c25cba3
Fabi <fabian.mast@gmx.de>
2023-06-24 21:13:17 +0200

    Results first draft

--- neuroapp/lib/views/routineView.dart
+++ neuroapp/lib/views/routineView.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:neuroapp/views/resultView.dart';
 
 class RoutineView extends StatelessWidget {
-
   const RoutineView({Key? key}) : super(key: key);
 
   @override
@@ -12,9 +12,24 @@ class RoutineView extends StatelessWidget {
         title: Text('Routine View'),
       ),
       body: Center(
-        child: Text(
-          'hello',
-          style: TextStyle(fontSize: 18),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'hello',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ResultView()),
+                );
+              },
+              child: Text('Go to Result View'),
+            ),
+          ],
         ),
       ),
     );
